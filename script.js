1a7646421b7f22643420899882c429d502b6b5b5
Ismailmirzoev <mirzoevismail391@gmail.com>
2025-03-07 20:53:52 +0400

    m

---
+++ script.js
@@ -0,0 +1,28 @@
+let object1 = {
+    marka: "Apple",
+    model: "16 pro",
+    memory: "1TB",
+    color: "black"
+}
+let object2 = {
+    marka: "Apple",
+    model: "15 pro",
+    memory: "256gb",
+    color: "blue"
+}
+let object3 = {
+    marka: "Apple",
+    model: "14 pro",
+    memory: "512gb",
+    color: "white"
+}
+let pr1 = prompt("Plese buy apple");
+if (pr1 == "marka apple model 16 pro memory 1tb") {
+    alert("color is black");
+}
+if (pr1 == "marka apple model 15 pro memory 256gb") {
+    alert("color is black");
+}
+if (pr1 == "marka apple model 14 pro memory 512gb") {
+    alert("color is black");
+}
