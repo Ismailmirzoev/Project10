92d4639c29f70e8d94b0e4ba067f3ffbf5846350
Ismailmirzoev <mirzoevismail391@gmail.com>
2025-03-08 12:30:24 +0400

    m

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-let object1 = {
-    marka: "Apple",
-    model: "16 pro",
-    memory: "1TB",
-    color: "black"
-}
-let object2 = {
-    marka: "Apple",
-    model: "15 pro",
-    memory: "256gb",
-    color: "blue"
-}
-let object3 = {
-    marka: "Apple",
-    model: "14 pro",
-    memory: "512gb",
-    color: "white"
-}
-let pr1 = prompt("Plese buy apple");
-if (pr1 == "marka apple model 16 pro memory 1tb") {
-    alert("color is black");
-}
-if (pr1 == "marka apple model 15 pro memory 256gb") {
-    alert("color is black");
-}
-if (pr1 == "marka apple model 14 pro memory 512gb") {
-    alert("color is black");
+let sel1 = document.getElementById("sel1");
+let sel2 = document.getElementById("sel2");
+let sel3 = document.getElementById("sel3");
+function func1() {
+    localStorage.clear();
+    localStorage.setItem("sel1", sel1.value)
+    localStorage.setItem("sel2", sel2.value)
+    localStorage.setItem("sel3", sel3.value)
 }
