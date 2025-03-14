ba86d701aea659f73c8f2efb2c8f3ff0c9b5ff97
Ismailmirzoev <mirzoevismail391@gmail.com>
2025-03-14 18:07:35 +0400

    ь

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let sel1 = document.getElementById("sel1");
-let sel2 = document.getElementById("sel2");
-let sel3 = document.getElementById("sel3");
-function func1() {
-    localStorage.clear();
-    localStorage.setItem("sel1", sel1.value)
-    localStorage.setItem("sel2", sel2.value)
-    localStorage.setItem("sel3", sel3.value)
+let grades = prompt("Напишите сколько у вас оценок в школе")
+let obj = {
+    name: "name",
+    age: "age",
+    special: "major",
+    grades: grades
+}
+console.log(JSON.stringify(obj));
+if (obj.grades < 10) {
+    console.log("ERROR");
 }
